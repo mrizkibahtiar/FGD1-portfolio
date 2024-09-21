d6d6121c135da98967de9bed6b679bc965572288
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-09-21 21:05:04 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'suse': ['SUSE', 'sans-serif']
+      },
+      keyframes: {
+        'walking': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        },
+        'goyang': {
+          '0%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+          '100%': { transform: 'rotate(-15deg)' },
+        }
+      },
+      animation: {
+        'walking': 'walking 10s linear infinite',
+        'goyang': 'goyang 2s linear infinite'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
 
